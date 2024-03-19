e11ba77def588a7897fdafde73b0f999a29ac1c7
German <germancanteros26@gmail.com>
2024-03-18 23:58:44 -0300

    Ultimo bug solucionado

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import {Navbar} from './navbar.jsx'
 import {Gallery} from './gallery.jsx'
 import {Vento} from './vento.jsx'
 import './main.css'
-import './animations.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
