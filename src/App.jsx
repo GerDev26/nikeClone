5caf323b0363afbdc9bc5da508323dd337986426
German <germancanteros26@gmail.com>
2024-03-19 21:39:21 -0300

    Solicitud fetch a api

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,40 @@ import {Gallery} from './gallery.jsx'
 import {Vento} from './vento.jsx'
 import './App.css'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function App (){
 
-/*     const [cocktails, setCocktails] = useState(null)
+    const [cocktails, setCocktails] = useState(null)
 
     useEffect(() => {
-    fetch("www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=b")
       .then(res => res.json())
       .then(data => {
+        console.log(data)
         setCocktails(data);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
         setCocktails({ error: 'Error fetching data' });
       });
-  }, []); */
+  }, []);
     return(
         <>
             <Navbar></Navbar>
             <Gallery>
-              <PreviewCard 
-                text="Air Force" 
-                imageUrl="img/zapas.png"
-                color="#ccb6a4">
-              </PreviewCard>
+                {cocktails ? (
+                    cocktails.drinks.map((cocktail, index) => (
+                      <PreviewCard 
+                        key={index}
+                        text={cocktail.strDrink}
+                        imageUrl={cocktail.strDrinkThumb}
+                        color="#ccb6a4"
+                      />
+                    ))
+                  ) : (
+                    <div>Cargando...</div>
+                  )}
             </Gallery>
 
             <Vento
